a2c60f8cdf9d5fa52b2d824e82d03a198adfb3cb
Jesus Guzman <jose1824@live.com.mx>
2020-06-10 21:52:58 -0500

    Gitignore

--- index.js
+++ index.js
@@ -1,115 +1,133 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+var restify = require('restify');
 
-// index.js is used to setup and configure your bot
+var builder = require('botbuilder');
 
-// Import required packages
-const path = require('path');
-const restify = require('restify');
+var botbuilder_azure = require("botbuilder-azure");
 
-// Import required bot services.
-// See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const { BotFrameworkAdapter, ConversationState, InputHints, MemoryStorage, UserState } = require('botbuilder');
+var builder_cognitiveservices = require("botbuilder-cognitiveservices");
 
-const { FlightBookingRecognizer } = require('./dialogs/flightBookingRecognizer');
+var inMemoryStorage = new builder.MemoryBotStorage();   
 
-// This bot's main dialog.
-const { DialogAndWelcomeBot } = require('./bots/dialogAndWelcomeBot');
-const { MainDialog } = require('./dialogs/mainDialog');
+// Setup Restify Server
 
-// the bot's booking dialog
-const { BookingDialog } = require('./dialogs/bookingDialog');
-const BOOKING_DIALOG = 'bookingDialog';
+var server = restify.createServer();
 
-// Note: Ensure you have a .env file and include LuisAppId, LuisAPIKey and LuisAPIHostName.
-const ENV_FILE = path.join(__dirname, '.env');
-require('dotenv').config({ path: ENV_FILE });
+server.listen(process.env.port || process.env.PORT || 3978, function () {
 
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about adapters.
-const adapter = new BotFrameworkAdapter({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
+console.log('%s listening to %s', server.name, server.url);
+
 });
 
-// Catch-all for errors.
-const onTurnErrorHandler = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights.
-    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+// Create chat connector for communicating with the Bot Framework Service
 
-    // Send a trace activity, which will be displayed in Bot Framework Emulator
-    await context.sendTraceActivity(
-        'OnTurnError Trace',
-        `${ error }`,
-        'https://www.botframework.com/schemas/error',
-        'TurnError'
-    );
+var connector = new builder.ChatConnector({
 
-    // Send a message to the user
-    let onTurnErrorMessage = 'The bot encountered an error or bug.';
-    await context.sendActivity(onTurnErrorMessage, onTurnErrorMessage, InputHints.ExpectingInput);
-    onTurnErrorMessage = 'To continue to run this bot, please fix the bot source code.';
-    await context.sendActivity(onTurnErrorMessage, onTurnErrorMessage, InputHints.ExpectingInput);
-    // Clear out state
-    await conversationState.delete(context);
-};
+appId: process.env.MicrosoftAppId,
 
-// Set the onTurnError for the singleton BotFrameworkAdapter.
-adapter.onTurnError = onTurnErrorHandler;
+appPassword: process.env.MicrosoftAppPassword,
 
-// Define a state store for your bot. See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
-// A bot requires a state store to persist the dialog and user state between messages.
+openIdMetadata: process.env.BotOpenIdMetadata
 
-// For local development, in-memory storage is used.
-// CAUTION: The Memory Storage used here is for local bot debugging only. When the bot
-// is restarted, anything stored in memory will be gone.
-const memoryStorage = new MemoryStorage();
-const conversationState = new ConversationState(memoryStorage);
-const userState = new UserState(memoryStorage);
-
-// If configured, pass in the FlightBookingRecognizer.  (Defining it externally allows it to be mocked for tests)
-const { LuisAppId, LuisAPIKey, LuisAPIHostName } = process.env;
-const luisConfig = { applicationId: LuisAppId, endpointKey: LuisAPIKey, endpoint: `https://${ LuisAPIHostName }` };
-
-const luisRecognizer = new FlightBookingRecognizer(luisConfig);
-
-// Create the main dialog.
-const bookingDialog = new BookingDialog(BOOKING_DIALOG);
-const dialog = new MainDialog(luisRecognizer, bookingDialog);
-const bot = new DialogAndWelcomeBot(conversationState, userState, dialog);
-
-// Create HTTP server
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function() {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
 
-// Listen for incoming activities and route them to your bot main dialog.
-server.post('/api/messages', (req, res) => {
-    // Route received a request to adapter for processing
-    adapter.processActivity(req, res, async (turnContext) => {
-        // route to bot activity handler.
-        await bot.run(turnContext);
-    });
+// Listen for messages from users
+
+server.post('/api/messages', connector.listen());
+
+var tableName = 'botdata';
+
+var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+
+var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+// Create your bot with a function to receive messages from the user
+
+var bot = new builder.UniversalBot(connector).set('storage', inMemoryStorage);
+
+bot.set('storage', tableStorage);
+
+// Recognizer and and Dialog for preview QnAMaker service
+
+var previewRecognizer = new builder_cognitiveservices.QnAMakerRecognizer({
+
+knowledgeBaseId: process.env.QnAKnowledgebaseId,
+
+authKey: process.env.QnAAuthKey || process.env.QnASubscriptionKey
+
 });
 
-// Listen for Upgrade requests for Streaming.
-server.on('upgrade', (req, socket, head) => {
-    // Create an adapter scoped to this WebSocket connection to allow storing session data.
-    const streamingAdapter = new BotFrameworkAdapter({
-        appId: process.env.MicrosoftAppId,
-        appPassword: process.env.MicrosoftAppPassword
-    });
-    // Set onTurnError for the BotFrameworkAdapter created for each connection.
-    streamingAdapter.onTurnError = onTurnErrorHandler;
+var basicQnAMakerPreviewDialog = new builder_cognitiveservices.QnAMakerDialog({
 
-    streamingAdapter.useWebSocket(req, socket, head, async (context) => {
-        // After connecting via WebSocket, run this logic for every request sent over
-        // the WebSocket connection.
-        await bot.run(context);
-    });
+recognizers: [previewRecognizer],
+
+defaultMessage: 'No match! Try changing the query terms!',
+
+qnaThreshold: 0.3
+
+}
+
+);
+
+bot.dialog('basicQnAMakerPreviewDialog', basicQnAMakerPreviewDialog);
+
+// Recognizer and and Dialog for GA QnAMaker service
+
+var recognizer = new builder_cognitiveservices.QnAMakerRecognizer({
+
+knowledgeBaseId: process.env.QnAKnowledgebaseId,
+
+authKey: process.env.QnAAuthKey || process.env.QnASubscriptionKey, // Backward compatibility with QnAMaker (Preview)
+
+endpointHostName: process.env.QnAEndpointHostName
+
 });
+
+var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
+
+recognizers: [recognizer],
+
+defaultMessage: "I'm not quite sure what you're asking. Please ask your question again.",
+
+qnaThreshold: 0.3
+
+});
+
+bot.dialog('basicQnAMakerDialog', basicQnAMakerDialog);
+
+bot.dialog('/', //basicQnAMakerDialog);
+
+[
+
+function (session) {
+
+var qnaKnowledgebaseId = process.env.QnAKnowledgebaseId;
+
+var qnaAuthKey = process.env.QnAAuthKey || process.env.QnASubscriptionKey;
+
+var endpointHostName = process.env.QnAEndpointHostName;
+
+// QnA Subscription Key and KnowledgeBase Id null verification
+
+if ((qnaAuthKey == null || qnaAuthKey == '') || (qnaKnowledgebaseId == null || qnaKnowledgebaseId == ''))
+
+session.send('Please set QnAKnowledgebaseId, QnAAuthKey and QnAEndpointHostName (if applicable) in App Settings. Learn how to get them at https://aka.ms/qnaabssetup.');
+
+else {
+
+if (endpointHostName == null || endpointHostName == '')
+
+// Replace with Preview QnAMakerDialog service
+
+session.replaceDialog('basicQnAMakerPreviewDialog');
+
+else
+
+// Replace with GA QnAMakerDialog service
+
+session.replaceDialog('basicQnAMakerDialog');
+
+}
+
+}
+
+]);
